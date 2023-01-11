c7082ac401ee45d5ebad44e4a840942cfafaabbc
pencil_2004 <pencil_2004@mail.ru>
2023-01-11 10:22:54 +0300

    День5-1

--- service/service/settings.py
+++ service/service/settings.py
@@ -131,3 +131,6 @@ LOGGING = {
     "handlers": {"console": {"class": "logging.StreamHandler"}},
     "loggers": {"django.db.backends": {"handlers": ["console"], "level": "DEBUG"}},
 }
+
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
